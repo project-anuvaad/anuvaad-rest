0a3525028c7730ee3eb28254d60eb4c46fe22668
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2019-10-03 19:28:31 +0530

    Update build for apigw

---
+++ build_apigw.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+commit_id=$(git rev-parse --short HEAD)
+echo $commit_id> commit_id.txt
+docker build -t gohila/$image_name:$commit_id .
+docker login -u $dockerhub_user -p $dockerhub_pass
+docker push gohila/$image_name:$commit_id
+
